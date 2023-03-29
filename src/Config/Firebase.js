dfbe7d6e394869a8b1d5a908cfda27e456d77a09
Eby-Tom <ebytomy7@gmail.com>
2023-03-29 18:59:17 +0530

    dhkv

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -6,45 +6,47 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 export const FireContext = createContext()
 
 const Firebase = (props) => {
-    const [isapion, setIsapion] = useState(false)
-    const [user, setUser] = useState(null)
+  const [isapion, setIsapion] = useState(false)
+  const [user, setUser] = useState(null)
 
-    useEffect(() => {
-        let app = null
-        const firebaseConfig = {
-          apiKey: "AIzaSyBgaiX0lXGlB0UWfTmESvb3Gykywy8yp1k",
-          authDomain: "service-learning-3bb28.firebaseapp.com",
-          databaseURL: "https://service-learning-3bb28-default-rtdb.asia-southeast1.firebasedatabase.app",
-          projectId: "service-learning-3bb28",
-          storageBucket: "service-learning-3bb28.appspot.com",
-          messagingSenderId: "810147190526",
-          appId: "1:810147190526:web:06ecf3e3d35f4910e077af"
-        };
-        if(app==null){
-          app = initializeApp(firebaseConfig);
-          setIsapion(true)
-        }
-        const analytics = getAnalytics(app);
-      }, []);
+  useEffect(() => {
+    let app = null
+    const firebaseConfig = {
+      apiKey: "AIzaSyBgaiX0lXGlB0UWfTmESvb3Gykywy8yp1k",
+      authDomain: "service-learning-3bb28.firebaseapp.com",
+      databaseURL: "https://service-learning-3bb28-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "service-learning-3bb28",
+      storageBucket: "service-learning-3bb28.appspot.com",
+      messagingSenderId: "810147190526",
+      appId: "1:810147190526:web:06ecf3e3d35f4910e077af"
+    };
+    if (app == null) {
+      app = initializeApp(firebaseConfig);
+      setIsapion(true)
+    }
+    const analytics = getAnalytics(app);
+  }, []);
 
-      useEffect(() => {
-        if(!isapion) return
+  useEffect(() => {
+    if (!isapion) return
 
-        onAuthStateChanged(getAuth(), (user) => {
-          console.log(user);
-          setUser(user)
-        })
-      }, [isapion])
-      
-      if(!isapion)
-      {
-        return <div>
-          ....
-        </div>
-      }
+    onAuthStateChanged(getAuth(), (user) => {
+      console.log(user);
+      setUser(user)
+    })
+  }, [isapion])
+
+  if (!isapion) {
+    return <div className='loadingPage'>
+      <div className='center'>
+        <img src="rpms.png" className='logoloader' />
+        <p className='rpmsHeading'>RPMS</p>
+      </div>
+    </div>
+  }
   return (
-    <FireContext.Provider value={{user}}>
-        {props.children}
+    <FireContext.Provider value={{ user }}>
+      {props.children}
     </FireContext.Provider>
   )
 }
