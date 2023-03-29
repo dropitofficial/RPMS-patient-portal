876f5a9801081ac5a09d5fa689154842580ff613
Eby-Tom <ebytomy7@gmail.com>
2023-03-29 22:44:29 +0530

    dhkv

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,29 +1,73 @@
 import React, { useState } from 'react'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { Link, useNavigate } from 'react-router-dom'
 const Login = () => {
 
     const navigate = useNavigate()
     const [isSignedup, setisSignedup] = useState(true)
     const [email, setemail] = useState("")
+    const [age, setage] = useState("")
+    const [phone, setphone] = useState("")
+    const [dispName, setdispName] = useState("")
     const [password, setpassword] = useState("")
     const [isloader, setisloader] = useState(false)
+    const [regerr, setregerr] = useState(false)
+    const [loginerr, setloginerr] = useState(false)
+    const [wrongCred, setwrongCred] = useState(false)
 
     // Initialize Firebase Authentication and get a reference to the service
     const auth = getAuth();
 
+    const verifyLogin =()=>{
+        setloginerr(false);
+        setwrongCred(false)
+        if(email==""||password=="")
+        {
+            setloginerr(true)
+        }
+        else{
+            login()
+        }
+    }
+
+    const verifyRegister =()=>{
+        setregerr(false)
+        setwrongCred(false)
+        if(dispName==""||phone==""||age==""||email==""||password=="")
+        {
+            setregerr(true)
+        }
+        else{
+            register()
+        }
+    }
+
     const register = () => {
+        setisloader(true);
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in 
                 const user = userCredential.user;
-                // ...
+                updateProfile(auth.currentUser, {
+                    displayName: dispName, photoURL: age+"#"+phone
+                }).then(() => {
+                    console.log("inserted@!!@");
+                }).catch((error) => {
+                    // An error occurred
+                    // ...
+                });
+                setisloader(false);
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                setisloader(false);
+                setwrongCred(true);
                 // ..
             });
+
+
+
     }
 
     const login = () => {
@@ -39,6 +83,7 @@ const Login = () => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 setisloader(false);
+                setwrongCred(true);
             });
     }
 
@@ -50,7 +95,14 @@ const Login = () => {
 
                         <div className="font-22px">Login</div>
                         <p><b>Remote Patient Monitoring System</b></p>
-
+                        {
+                            loginerr&&
+                            (<h3 className='errmssg'>Enter all feilds to continue</h3>)
+                        }
+                        {
+                            wrongCred&&
+                            <h3 className='errmssg'>Check Credentials</h3>
+                        }
                         <input placeholder="Enter email"
                             name="username"
                             type="text"
@@ -64,7 +116,7 @@ const Login = () => {
                             type="password"
                             className='passinput'
                         />
-                        <div onClick={() => login()} className='passinput btnn'>
+                        <div onClick={() => verifyLogin()} className='passinput btnn'>
                             {isloader ?
                                 <div className="loader"></div>
                                 :
@@ -85,7 +137,35 @@ const Login = () => {
 
                         <div className="font-22px">Register</div>
                         <p><b>Remote Patient Monitoring System</b></p>
-
+                        {
+                            regerr&&
+                            <h3 className='errmssg'>Enter all feilds to continue</h3>
+                        }
+                        {
+                            wrongCred&&
+                            <h3 className='errmssg'>Check Credentials</h3>
+                        }
+                        <input placeholder="Enter display name"
+                            name="password"
+                            type="text"
+                            value={dispName}
+                            onChange={(e) => setdispName(e.target.value)}
+                            className='passinput'
+                        />
+                        <input placeholder="Enter age"
+                            name="password"
+                            type="text"
+                            value={age}
+                            onChange={(e) => setage(e.target.value)}
+                            className='passinput'
+                        />
+                        <input placeholder="Enter phone number"
+                            name="password"
+                            type="text"
+                            value={phone}
+                            onChange={(e) => setphone(e.target.value)}
+                            className='passinput'
+                        />
                         <input placeholder="Enter username"
                             name="username"
                             type="text"
@@ -95,12 +175,16 @@ const Login = () => {
                         <input placeholder="Enter password"
                             name="password"
                             type="password"
-                            value={email}
+                            value={password}
                             onChange={(e) => setpassword(e.target.value)}
                             className='passinput'
                         />
-                        <div onClick={() => register()} className='passinput btnn'>
-                            <b>Register</b>
+                        <div onClick={() => verifyRegister()} className='passinput btnn'>
+                            {isloader ?
+                                <div className="loader"></div>
+                                :
+                                <b>Register</b>
+                            }
                         </div>
 
                         <div>
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { Link } from "react-router-dom"
-
+import { FireContext } from '../Config/Firebase';
 import { ChevronRightIcon, ShareAndroidIcon, SyncIcon } from "@primer/octicons-react"
 import Header from "../Components/Header"
 import { getAuth, signOut } from "firebase/auth"
@@ -8,40 +8,56 @@ import { getAuth, signOut } from "firebase/auth"
 
 const Profile = () => {
 
+    const { user } = useContext(FireContext)
+    const [bookAppo, setbookAppo] = useState(false)
+
     return (
         <div>
             <Header />
-        <div className="notification-page pos-r">
-            <div className="notification profile">
-                <img className="userlogo" src="user.png" />
-                <div className="profile-head">
-                    <h1>EBY TOM</h1>
-                    <b>ebytom</b>
+            <div className="notification-page pos-r">
+                <div className="notification profile">
+                    <img className="userlogo" src="user.png" />
+                    <div className="profile-head">
+                        <h1>{user.displayName}</h1>
+                        <b>{user.uid}</b>
+                    </div>
+                    {console.log(user)}
+                    <div className="user-details">
+                        <b>{user.photoURL.split('#')[0]}</b>
+                    </div>
+                    <div className="user-details">
+                        <b>{user.email}</b>
+                    </div>
+                    <div className="user-details">
+                        <b>{user.photoURL.split('#')[1]}</b>
+                    </div>
+                    <div className="docDetails">
+                        <div><b>Dr. Lokesh</b></div>
+                        <div><b>HK Hospital, Kengeri</b></div>
+                        {
+                            bookAppo ?
+                                <div>
+                                    <input className="cal" type="date" />
+                                    <div className="dispflex">
+                                        <div className="appoBtn">Book</div>
+                                        <div onClick={()=>setbookAppo(false)} className="appoBtn red">Cancel</div>
+                                    </div>
+                                </div>
+                                :
+                                <div className="appoBtn" onClick={() => setbookAppo(true)} >Book Appointment</div>
+                        }
+                    </div>
+                    <div className="logoutbtn"><div onClick={() => {
+                        signOut(getAuth()).then(() => {
+                            // Sign-out successful.
+                            console.log("logged ou");
+                        }).catch((error) => {
+                            // An error happened.
+                        });
+                    }}><b>Logout</b></div></div>
                 </div>
-                <div className="user-details">
-                    <b>90</b>
-                </div>
-                <div className="user-details">
-                    <b>eby@gmail.com</b>
-                </div>
-                <div className="user-details">
-                    <b>9856412563</b>
-                </div>
-                <div className="docDetails">
-                    <div><b>Dr. Lokesh</b></div>
-                    <div><b>HK Hospital, Kengeri</b></div>
-                </div>
-                <div className="logoutbtn"><div onClick={()=>{
-                    signOut(getAuth()).then(() => {
-                        // Sign-out successful.
-                        console.log("logged ou");
-                      }).catch((error) => {
-                        // An error happened.
-                      });
-                }}><b>Logout</b></div></div>
+
             </div>
-            
-        </div>
         </div>
     )
 }
